5c708ae90ca2dadf1fab3ade4c3e74cfde8e474a
nmtrung <trungnm@hvttgroup.com>
2024-05-18 22:56:06 +0700

    update

--- main.js
+++ main.js
@@ -28,7 +28,6 @@ function myFunction() {
   }
 
 
-  
 function reveal() {
 var reveals = document.querySelectorAll(".reveal");
   
@@ -46,3 +45,29 @@ for (var i = 0; i < reveals.length; i++) {
 }
   
 window.addEventListener("scroll", reveal);
+
+
+function updateTimer() {
+  future  = Date.parse("June 30, 2024 11:00:00");
+  now     = new Date();
+  diff    = future - now;
+
+  days  = Math.floor( diff / (1000*60*60*24) );
+  hours = Math.floor( diff / (1000*60*60) );
+  mins  = Math.floor( diff / (1000*60) );
+  secs  = Math.floor( diff / 1000 );
+
+  d = days;
+  h = hours - days  * 24;
+  m = mins  - hours * 60;
+  s = secs  - mins  * 60;
+
+  document.getElementById("timer").innerHTML = `
+    <div>${d}<span>ngày</span></div>
+    <div>${h}<span>giờ</span></div>
+    <div>${m}<span>phút</span></div>
+    <div>${s}<span>giây</span></div>
+  `;
+
+}
+setInterval('updateTimer()', 1000 );
